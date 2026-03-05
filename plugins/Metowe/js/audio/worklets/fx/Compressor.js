c5baaf49f7e4b9ca720aefd66fb7ee18f6d3d780
ivangaconsultingbase-Rel <ivangaconsultingbase@gmail.com>
2026-03-05 13:00:51 +0100

    Update Compressor.js

--- plugins/Metowe/js/audio/worklets/fx/Compressor.js
+++ plugins/Metowe/js/audio/worklets/fx/Compressor.js
@@ -1,1 +1,19 @@
+export class Compressor{
 
+constructor(ctx){
+
+this.input=ctx.createDynamicsCompressor()
+this.output=this.input
+
+this.input.threshold.value=-18
+this.input.ratio.value=4
+
+}
+
+connect(node){
+
+this.output.connect(node)
+
+}
+
+}
