3678b6dab2a091915d219ab2c8723cde0cdb480e
ivangaconsultingbase-Rel <ivangaconsultingbase@gmail.com>
2026-03-05 19:37:18 +0100

    Update main.js

--- plugins/Metowe/js/main.js
+++ plugins/Metowe/js/main.js
@@ -2,14 +2,29 @@ import { SynthEngine } from "./audio/SynthEngine.js";
 import { Sequencer } from "./sequencer/Sequencer.js";
 import { UI } from "./ui/UI.js";
 
+async function boot(){
+
 const synth = new SynthEngine();
 await synth.init();
 
-const seq = new Sequencer(synth, (step) => ui.onStepChange(step));
+let ui;
 
-const ui = new UI(synth, seq);
+const seq = new Sequencer(
+synth,
+(step)=>ui?.onStepChange(step)
+);
 
-// Important sur mobile : unlock audio au premier geste
-window.addEventListener("pointerdown", () => synth.resume(), { once: true });
+ui = new UI(synth,seq);
+
+// unlock audio mobile
+window.addEventListener(
+"pointerdown",
+()=>synth.resume(),
+{once:true}
+);
 
 ui.init();
+
+}
+
+boot();
