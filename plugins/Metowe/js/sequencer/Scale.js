b494300a0d5d4e56dde7929082550811a5dadbd7
ivangaconsultingbase-Rel <ivangaconsultingbase@gmail.com>
2026-03-05 13:10:27 +0100

    Update Scale.js

--- plugins/Metowe/js/sequencer/Scale.js
+++ plugins/Metowe/js/sequencer/Scale.js
@@ -1,3 +1,10 @@
-export const MinorScale=[
-0,2,3,5,7,8,10
-]
+export const SCALES = {
+  aeolian:        [0,2,3,5,7,8,10],
+  dorian:         [0,2,3,5,7,9,10],
+  phrygian:       [0,1,3,5,7,8,10],
+  harmonicMinor:  [0,2,3,5,7,8,11],
+  melodicMinor:   [0,2,3,5,7,9,11],
+  minorPent:      [0,3,5,7,10],
+};
+
+export const DEG_NAMES = ["1","2","b3","4","5","b6","b7","8","9","#9","10","11","b13","13","b7","8"];
