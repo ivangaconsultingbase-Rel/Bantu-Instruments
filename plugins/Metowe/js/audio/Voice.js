2a5db157ffa77bdd250ef57ce81c2f40f071dd3d
ivangaconsultingbase-Rel <ivangaconsultingbase@gmail.com>
2026-03-06 17:44:00 +0100

    Update Voice.js

--- plugins/Metowe/js/audio/Voice.js
+++ plugins/Metowe/js/audio/Voice.js
@@ -1,53 +1,114 @@
-export class Voice{
+export class Voice {
+  constructor(ctx, out, engine, midi, velocity, time, length){
+    this.ctx = ctx;
+    this.out = out;
+    this.engine = engine;
 
-constructor(ctx,out,note,vel,time,length,engine){
+    this.midi = midi;
+    this.velocity = velocity;
+    this.time = time;
+    this.length = length;
+  }
 
-this.ctx=ctx
-this.out=out
+  mtof(n){
+    return 440 * Math.pow(2, (n - 69) / 12);
+  }
 
-this.note=note
-this.vel=vel
+  start(){
+    const e = this.engine;
+    const ctx = this.ctx;
+    const t0 = this.time;
+    const tOff = t0 + Math.max(0.02, this.length);
 
-this.time=time||ctx.currentTime
-this.length=length
+    // OSC1
+    const o1 = ctx.createOscillator();
+    o1.type = e.osc1Type;
+    o1.frequency.setValueAtTime(this.mtof(this.midi), t0);
 
-this.engine=engine
+    // OSC2 (detune)
+    const o2 = ctx.createOscillator();
+    o2.type = e.osc2Type;
+    o2.frequency.setValueAtTime(this.mtof(this.midi), t0);
+    o2.detune.setValueAtTime(e.detune, t0);
 
-}
+    // SUB (1 octave lower, square)
+    const sub = ctx.createOscillator();
+    sub.type = "square";
+    sub.frequency.setValueAtTime(this.mtof(this.midi - 12), t0);
 
-freq(n){
+    // NOISE (looped buffer)
+    const noiseSrc = ctx.createBufferSource();
+    noiseSrc.buffer = this._noiseBuffer(ctx);
+    noiseSrc.loop = true;
 
-return 440*Math.pow(2,(n-69)/12)
+    // mixer gains
+    const g1 = ctx.createGain(); g1.gain.value = 0.60;
+    const g2 = ctx.createGain(); g2.gain.value = 0.55;
+    const gSub = ctx.createGain(); gSub.gain.value = e.subMix;
+    const gNoise = ctx.createGain(); gNoise.gain.value = e.noiseMix;
 
-}
+    // filter
+    const vcf = ctx.createBiquadFilter();
+    vcf.type = "lowpass";
+    vcf.Q.setValueAtTime(0.5 + e.res * 18, t0);
 
-start(){
+    // env -> VCF
+    const baseCutoff = e.cutoff;
+    const peakCutoff = Math.min(12000, baseCutoff * (1 + 2.2 * e.envAmt));
 
-const osc=this.ctx.createOscillator()
+    vcf.frequency.setValueAtTime(baseCutoff, t0);
+    vcf.frequency.linearRampToValueAtTime(peakCutoff, t0 + e.a);
+    vcf.frequency.linearRampToValueAtTime(
+      baseCutoff + (peakCutoff - baseCutoff) * e.s,
+      t0 + e.a + e.d
+    );
+    vcf.frequency.setValueAtTime(
+      baseCutoff + (peakCutoff - baseCutoff) * e.s,
+      tOff
+    );
+    vcf.frequency.linearRampToValueAtTime(baseCutoff, tOff + e.r);
 
-osc.type=this.engine.wave
+    // amp env
+    const vca = ctx.createGain();
+    const vel = this.velocity;
+    vca.gain.setValueAtTime(0.0001, t0);
+    vca.gain.linearRampToValueAtTime(vel, t0 + e.a);
+    vca.gain.linearRampToValueAtTime(Math.max(0.0001, vel * e.s), t0 + e.a + e.d);
+    vca.gain.setValueAtTime(Math.max(0.0001, vel * e.s), tOff);
+    vca.gain.linearRampToValueAtTime(0.0001, tOff + e.r);
 
-osc.frequency.value=this.freq(this.note)
+    // route
+    o1.connect(g1); o2.connect(g2);
+    sub.connect(gSub);
+    noiseSrc.connect(gNoise);
 
-const filter=this.ctx.createBiquadFilter()
+    g1.connect(vcf);
+    g2.connect(vcf);
+    gSub.connect(vcf);
+    gNoise.connect(vcf);
 
-filter.type="lowpass"
-filter.frequency.value=this.engine.cutoff
-filter.Q.value=this.engine.res*12
+    vcf.connect(vca);
+    vca.connect(this.out);
 
-const gain=this.ctx.createGain()
+    // start/stop
+    o1.start(t0); o2.start(t0);
+    sub.start(t0);
+    noiseSrc.start(t0);
 
-gain.gain.setValueAtTime(0.0001,this.time)
-gain.gain.linearRampToValueAtTime(this.vel,this.time+0.01)
-gain.gain.exponentialRampToValueAtTime(0.0001,this.time+this.length)
+    const stopAt = tOff + e.r + 0.05;
+    o1.stop(stopAt); o2.stop(stopAt);
+    sub.stop(stopAt);
+    noiseSrc.stop(stopAt);
+  }
 
-osc.connect(filter)
-filter.connect(gain)
-gain.connect(this.out)
-
-osc.start(this.time)
-osc.stop(this.time+this.length)
-
-}
-
+  _noiseBuffer(ctx){
+    // simple cached white noise buffer
+    if (ctx.__noiseBuf) return ctx.__noiseBuf;
+    const len = ctx.sampleRate * 1.0;
+    const buf = ctx.createBuffer(1, len, ctx.sampleRate);
+    const ch = buf.getChannelData(0);
+    for (let i=0;i<len;i++) ch[i] = (Math.random()*2-1) * 0.5;
+    ctx.__noiseBuf = buf;
+    return buf;
+  }
 }
