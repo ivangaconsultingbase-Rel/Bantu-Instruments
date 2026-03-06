c897bb24e7f640ccf33e1d19ead3738fa1bebbbc
ivangaconsultingbase-Rel <ivangaconsultingbase@gmail.com>
2026-03-06 17:42:30 +0100

    Update main.js

--- plugins/Metowe/js/main.js
+++ plugins/Metowe/js/main.js
@@ -1,24 +1,19 @@
-import {SynthEngine} from "./audio/SynthEngine.js"
-import {Sequencer} from "./sequencer/Sequencer.js"
-import {UI} from "./ui/UI.js"
+import { SynthEngine } from "./audio/SynthEngine.js";
+import { Sequencer } from "./sequencer/Sequencer.js";
+import { UI } from "./ui/UI.js";
 
-const synth=new SynthEngine()
+const synth = new SynthEngine();
+let ui = null;
 
-let ui
+const seq = new Sequencer(synth, (step) => {
+  ui?.onStep(step);
+});
 
-const sequencer=new Sequencer(
-synth,
-(step)=>ui.onStepChange(step)
-)
-
-ui=new UI(synth,sequencer)
+ui = new UI(synth, seq);
 
 async function boot(){
-
-await synth.init()
-
-ui.init()
-
+  await synth.init();
+  ui.init();
 }
 
-boot()
+boot();
